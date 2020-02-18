680f92c852ecb2839bf825a75c6d07f9ca2257b0
Sylvain Rabot <s.rabot@lectra.com>
2020-02-18 17:06:11 +0100

    Add provider debugging with delve
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- plugin/client.go
+++ plugin/client.go
@@ -1,8 +1,12 @@
 package plugin
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"time"
 
 	hclog "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
@@ -23,14 +27,85 @@ func ClientConfig(m discovery.PluginMeta) *plugin.ClientConfig {
 		Output: os.Stderr,
 	})
 
+	var cmdPath string
+	var cmdArgs []string
+	clientTimeout := 60 * time.Second
+
+	// Plugin debugging using DELVE
+	// Env variables available to configure the debugger:
+	// - TF_PLUGIN_DELVE file path to the delve executable
+	// - TF_PLUGIN_DELVE_LISTEN_ADDR address delve will be listening (default: "")
+	// - TF_PLUGIN_DELVE_LISTEN_PORT port delve will be listening (default: 2345)
+	// - TF_PLUGIN_DELVE_API_VERSION api version to use for remote debugging (default: 2)
+	tfPluginDelve := os.Getenv("TF_PLUGIN_DELVE")
+	if len(tfPluginDelve) > 0 {
+		info, err := os.Stat(tfPluginDelve)
+
+		if os.IsNotExist(err) {
+			log.Fatalf("TF_PLUGIN_DELVE=%s does not exist", tfPluginDelve)
+		} else if info.Mode()&0111 == 0 {
+			log.Fatalf("TF_PLUGIN_DELVE=%s is not executable", tfPluginDelve)
+		} else {
+			pluginDelveAddr := "127.0.0.1"
+			pluginDelvePort := 2345
+			pluginDelveAPIVersion := 2
+			// pluginDelveLogDest := "/dev/console"
+
+			envPluginDelveAddr := os.Getenv("TF_PLUGIN_DELVE_LISTEN_ADDR")
+			envPluginDelvePort := os.Getenv("TF_PLUGIN_DELVE_LISTEN_PORT")
+			envPluginDelveAPIVersion := os.Getenv("TF_PLUGIN_DELVE_API_VERSION")
+			// envPluginDelveLogDest := os.Getenv("TF_PLUGIN_DELVE_LOG_DEST")
+
+			if len(envPluginDelveAddr) > 0 {
+				pluginDelveAddr = envPluginDelveAddr
+			}
+
+			if len(envPluginDelvePort) > 0 {
+				port, err := strconv.Atoi(envPluginDelvePort)
+				if err == nil {
+					pluginDelvePort = port
+				} else {
+					log.Fatalf("TF_PLUGIN_DELVE_LISTEN_PORT=%v is invalid", envPluginDelvePort)
+				}
+			}
+
+			if len(envPluginDelveAPIVersion) > 0 {
+				apiVersion, err := strconv.Atoi(envPluginDelveAPIVersion)
+				if err == nil && apiVersion >= 1 && apiVersion <= 2 {
+					pluginDelveAPIVersion = apiVersion
+				} else {
+					log.Fatalf("TF_PLUGIN_DELVE_API_VERSION=%v is invalid (must be 1 or 2)", envPluginDelveAPIVersion)
+				}
+			}
+
+			// if len(envPluginDelveLogDest) > 0 {
+			// 	pluginDelveLogDest = envPluginDelveLogDest
+			// }
+
+			clientTimeout = 10 * time.Minute
+			cmdPath = tfPluginDelve
+			cmdArgs = []string{
+				"exec",
+				"--headless",
+				"--listen=" + fmt.Sprintf("%s:%d", pluginDelveAddr, pluginDelvePort),
+				"--api-version=" + fmt.Sprintf("%d", pluginDelveAPIVersion),
+				"--",
+				m.Path,
+			}
+		}
+	} else {
+		cmdPath = m.Path
+	}
+
 	return &plugin.ClientConfig{
-		Cmd:              exec.Command(m.Path),
+		Cmd:              exec.Command(cmdPath, cmdArgs...),
 		HandshakeConfig:  Handshake,
 		VersionedPlugins: VersionedPlugins,
 		Managed:          true,
 		Logger:           logger,
 		AllowedProtocols: []plugin.Protocol{plugin.ProtocolGRPC},
 		AutoMTLS:         enableAutoMTLS,
+		StartTimeout:     clientTimeout,
 	}
 }
 
